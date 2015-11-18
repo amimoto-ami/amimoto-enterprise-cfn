d75383231c7efd6d886eddefeec0a567c8272247
hideokamoto <hide.okamoto@digitalcube.jp>
2015-11-18 19:27:19 +0900

    initial create vpc

---
+++ create-vpc.rb
@@ -0,0 +1,5 @@
+AWSTemplateFormatVersion "2010-09-09"
+
+Resources do
+  _include 'include/vpc/vpc.rb'
+end
---
+++ include/vpc/vpc.rb
@@ -0,0 +1,6 @@
+VPC do
+  Type "AWS::EC2::VPC"
+  Properties do
+    CidrBlock "10.0.0.0/16"
+  end
+end
