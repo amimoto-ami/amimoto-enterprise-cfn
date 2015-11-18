55a144fb87bec40852a891008a5aa5cff41103ee
hideokamoto <hide.okamoto@digitalcube.jp>
2015-11-18 19:56:16 +0900

    Update VPC templates
    Result
    Stack Resource Summaries:
    [
      {
        "LogicalResourceId": "VPC",
        "PhysicalResourceId": "vpc-d83f1cbd",
        "ResourceType": "AWS::EC2::VPC",
        "ResourceStatus": "UPDATE_COMPLETE",
        "ResourceStatusReason": null,
        "LastUpdatedTimestamp": "2015-11-18 10:55:00 UTC"
      }
    ]
    
    Outputs:
    {
    }

--- create-vpc.rb
+++ create-vpc.rb
@@ -1,4 +1,7 @@
 AWSTemplateFormatVersion "2010-09-09"
+Description (<<-EOS).undent
+  This is Simple VPC Formation
+EOS
 
 Resources do
   _include 'include/vpc/vpc.rb'
--- include/vpc/vpc.rb
+++ include/vpc/vpc.rb
@@ -2,5 +2,20 @@ VPC do
   Type "AWS::EC2::VPC"
   Properties do
     CidrBlock "10.0.0.0/16"
+    InstanceTenancy "default"
+    EnableDnsSupport "true"
+    EnableDnsHostnames "true"
+    Tags [
+      _{
+        Key "Name"
+        Value "SampleVPC"
+      },
+      _{
+        Key "Application"
+        Value do
+          Ref "AWS::StackName"
+        end
+      }
+    ]
   end
 end
