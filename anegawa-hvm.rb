7dd4eec07350a4ef76ed233bf3755fa5e0533a3d
hideokamoto <hide.okamoto@digitalcube.jp>
2015-12-01 12:17:36 +0900

    Add CloudFront Invalidation IAM Role & Access Keys

--- anegawa-hvm.rb
+++ anegawa-hvm.rb
@@ -39,16 +39,28 @@ Outputs do
       ]
     end
   end
-  InstanceId do
+  EC2InstanceId do
     Description "Access for the first time, Please input it."
     Value do
       Ref "EC2"
     end
   end
-  DistributionId do
+  CloudFrontDistributionId do
     Description "This is CloudFront Disctribution ID. Please set 'C3 CloudFlont Cache Clear' plugin on admin page."
     Value do
       Ref "CloudFront"
+    end
+  end
+  CloudFrontAccessKey do
+    Description "This is CloudFront Access Key. Please set 'C3 CloudFlont Cache Clear' plugin on admin page."
+    Value do
+      Ref "IAMCfInvalidationAccessKey"
+    end
+  end
+  CloudFrontSecretKey do
+    Description "This is CloudFront Secret Key. Please set 'C3 CloudFlont Cache Clear' plugin on admin page."
+    Value do
+      Fn__GetAtt "IAMCfInvalidationAccessKey", "SecretAccessKey"
     end
   end
 end
--- include/iam/cf-invalidation.rb
+++ include/iam/cf-invalidation.rb
@@ -17,3 +17,24 @@ IAMCfInvalidationRole do
     Path "/"
   end
 end
+
+IAMCfInvalidationAccessKey do
+  Type "AWS::IAM::AccessKey"
+  Properties do
+    Status "Active"
+    UserName do
+      Ref "IAMCfInvalidationUser"
+    end
+  end
+end
+
+IAMCfInvalidationUser do
+  Type "AWS::IAM::User"
+  Properties do
+    Path "/"
+    ManagedPolicyArns [
+      "arn:aws:iam::aws:policy/CloudFrontFullAccess",
+      "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    ]
+  end
+end
