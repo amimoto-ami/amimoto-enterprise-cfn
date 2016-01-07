66a40595d030d6b9c4c7cc88c4ffc83e6271d0d6
sawanoboly <sawanoboriyu@higanworks.com>
2016-01-07 20:04:02 +0900

    Revert "update ami_hvm"
    This reverts commit 56f3e9d9fbce5496972a1c8abc1f551f5d4f9d8c.

--- include/mapping/ami_hvm.rb
+++ include/mapping/ami_hvm.rb
@@ -1,33 +1,14 @@
 MPAmimotov4(
-{
-  "eu-west-1": {
-    "AMI": "ami-2b75a958"
-  },
-  "ap-southeast-1": {
-    "AMI": "ami-ff86419c"
-  },
-  "ap-southeast-2": {
-    "AMI": "ami-7085da13"
-  },
-  "eu-central-1": {
-    "AMI": "ami-a6bcacca"
-  },
-  "ap-northeast-2": {
-    "AMI": "ami-259d534b"
-  },
-  "ap-northeast-1": {
-    "AMI": "ami-e6da8088"
-  },
-  "us-east-1": {
-    "AMI": "ami-7c166716"
-  },
-  "sa-east-1": {
-    "AMI": "ami-12338b7e"
-  },
-  "us-west-1": {
-    "AMI": "ami-b2b4d8d2"
-  },
-  "us-west-2": {
-    "AMI": "ami-2874991b"
-  }
-})
+  {
+  "us-east-1"=>{"AMI"=>"ami-7c166716", "Location"=>"Virginia"},
+  "us-west-2"=>{"AMI"=>"ami-2874991b", "Location"=>"Oregon"},
+  "us-west-1"=>{"AMI"=>"ami-b2b4d8d2", "Location"=>"N.California"},
+  "eu-central-1"=>{"AMI"=>"ami-a6bcacca", "Location"=>"EU_Frankfurt"},
+  "eu-west-1"=>{"AMI"=>"ami-2b75a958", "Location"=>"EU_Ireland"},
+  "ap-southeast-1"=>{"AMI"=>"ami-ff86419c", "Location"=>"Singapore"},
+  "ap-southeast-2"=>{"AMI"=>"ami-7085da13", "Location"=>"Sydney"},
+  "ap-northeast-1"=>{"AMI"=>"ami-e6da8088", "Location"=>"Tokyo"},
+  "ap-northeast-2"=>{"AMI"=>"ami-259d534b", "Location"=>"Seoul"},
+  "sa-east-1"=>{"AMI"=>"ami-12338b7e", "Location"=>"Sao_Paul"}
+ }
+)
