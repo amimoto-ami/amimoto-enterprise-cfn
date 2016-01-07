d6d93ec33c58f4f373bca8f5ce2e2992e3229b6b
hideokamoto <hide.okamoto@digitalcube.jp>
2016-01-07 16:57:10 +0900

    remove unse output param

--- outputs/cloudfront.rb
+++ outputs/cloudfront.rb
@@ -18,15 +18,3 @@
       Ref "CloudFront"
     end
   end
-  CloudFrontAccessKey do
-    Description "This is CloudFront Access Key. Please set 'C3 CloudFlont Cache Clear' plugin on admin page."
-    Value do
-      Ref "IAMCfInvalidationAccessKey"
-    end
-  end
-  CloudFrontSecretKey do
-    Description "This is CloudFront Secret Key. Please set 'C3 CloudFlont Cache Clear' plugin on admin page."
-    Value do
-      Fn__GetAtt "IAMCfInvalidationAccessKey", "SecretAccessKey"
-    end
-  end
