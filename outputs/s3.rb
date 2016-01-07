2fd5ec0b67dc92012366ae70a88a51ccdcba50be
hideokamoto <hide.okamoto@digitalcube.jp>
2016-01-07 18:17:34 +0900

    fix following issues
    - delete unuse output params
    - fix typo
    - add new AZ

--- outputs/s3.rb
+++ outputs/s3.rb
@@ -5,15 +5,3 @@ S3MediaBucketURL do
     Fn__GetAtt "S3Media", "WebsiteURL"
   end
 end
-S3MediaAccessKey do
-  Description "This is S3 Access Key. Please set 'Nephila clavata' plugin on admin page."
-  Value do
-    Ref "IAMS3MediaAccessKey"
-  end
-end
-S3MediaSecretKey do
-  Description "This is S3 Secret Key. Please set 'Nephila clavata' plugin on admin page."
-  Value do
-    Fn__GetAtt "IAMS3MediaAccessKey", "SecretAccessKey"
-  end
-end
