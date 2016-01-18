fa20a3ccf7b25c569992786840bd24ec74867942
hideokamoto <hide.okamoto@digitalcube.jp>
2016-01-18 14:14:15 +0900

    update instance list

--- params/rds_settings.rb
+++ params/rds_settings.rb
@@ -17,7 +17,25 @@ RDSInstanceType do
   Description           "RDS Instance Type"
   Default               "db.m3.medium"
   Type                  "String"
-  AllowedValues         "db.t2.micro", "db.t2.small", "db.t2.medium", "db.m1.small", "db.m1.medium", "db.m1.large", "db.m1.xlarge", "db.m3.medium", "db.m3.large", "db.m3.xlarge", "db.m3.2xlarge", "db.m2.xlarge", "db.m2.2xlarge", "db.m2.4xlarge", "db.r3.large", "db.r3.xlarge", "db.r3.2xlarge", "db.r3.4xlarge", "db.r3.8xlarge"
+  AllowedValues         "db.t2.micro",
+				"db.t2.small",
+				"db.t2.medium",
+				"db.m1.small",
+				"db.m1.medium",
+				"db.m1.large",
+				"db.m1.xlarge",
+				"db.m3.medium",
+				"db.m3.large",
+				"db.m3.xlarge",
+				"db.m3.2xlarge",
+				"db.m2.xlarge",
+				"db.m2.2xlarge",
+				"db.m2.4xlarge",
+				"db.r3.large",
+				"db.r3.xlarge",
+				"db.r3.2xlarge",
+				"db.r3.4xlarge",
+				"db.r3.8xlarge"
   ConstraintDescription "Must be a valid RDS Instance Type."
 end
 
