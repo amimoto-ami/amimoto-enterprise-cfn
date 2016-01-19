5535c981da13151febea089b6f4a6744536c7fe3
hideokamoto <hide.okamoto@digitalcube.jp>
2016-01-19 18:52:34 +0900

    initial deploy CodeDeploy

--- include/codedeploy/deploy.rb
+++ include/codedeploy/deploy.rb
@@ -1,3 +1,20 @@
-CodeDeploy do
+_include "include/iam/roles/codedeploy.rb"
+
+AmimotoDeploy do
 	Type "AWS::CodeDeploy::Application"
+end
+
+AmimotoDeploymentGroup do
+	Type "AWS::CodeDeploy::DeploymentGroup"
+	Properties do
+		ApplicationName do
+			Ref "AmimotoDeploy"
+		end
+		#AutoScalingGroups [
+		# ASさせるときに設定する
+		#]
+		ServiceRoleArn do
+			Fn__GetAtt "IAMRoleForCodeDeploy", "Arn"
+		end
+	end
 end
---
+++ include/iam/policies/codedeploy.rb
@@ -0,0 +1,20 @@
+IAMPolicyForCodeDeploy do
+  Type "AWS::IAM::ManagedPolicy"
+  Properties do
+    Description "Allow Code Deploy"
+    PolicyDocument do
+      Version "2012-10-17"
+      Statement [
+        _{
+          ## See This page
+          ## https://docs.aws.amazon.com/ja_jp/codedeploy/latest/userguide/getting-started-walkthrough.html
+          Effect "Allow"
+          Action [
+            "ec2:*"
+          ]
+          Resource "*"
+        }
+      ]
+    end
+  end
+end
