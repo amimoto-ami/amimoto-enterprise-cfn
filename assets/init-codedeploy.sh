49083cd7b07870e1d6424cc2d0e6205417f7d191
hideokamoto <hide.okamoto@digitalcube.jp>
2016-01-20 20:41:38 +0900

    script update for Code Deploy
    - delete yum update ( Chef not working issue)
    - Configure Your Source Content( For Your Development env)

--- assets/init-codedeploy.sh
+++ assets/init-codedeploy.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-yum update -y
 cd /tmp
 AZ=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//g'`
 BUCKET="https://aws-codedeploy-${AZ}.s3.amazonaws.com/latest/install"
